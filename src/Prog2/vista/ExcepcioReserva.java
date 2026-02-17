d05b2d0f169feeee27e95b80358fcd2518162d80
JuditCurriuSabates <jcurrisa31@alumnes.ub.edu>
2026-02-17 13:17:46 +0100

    Hola

---
+++ src/Prog2/vista/ExcepcioReserva.java
@@ -0,0 +1,14 @@
+
+package Prog2.vista;
+
+/**
+ *
+ * @author lauraigual
+ */
+public class ExcepcioReserva extends Exception {
+
+    public ExcepcioReserva(String message) {
+        super(message);
+    }
+    
+}
